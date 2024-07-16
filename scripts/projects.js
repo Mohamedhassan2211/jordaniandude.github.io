1f41aee1ea318d03d8b6d38c288dd92ffef2ce01
Mohamed Hassan <mohamedhassan112003@gmail.com>
2024-07-16 18:17:39 +0300

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,13 @@
+var PROJECTS=[
+    {
+        img:"project1.png",
+        title:"project1",
+        description:"This is website project "
+    },
+    {
+        img:"project2.png",
+        title:"project2",
+        description:"This is Mars Rover project "
+    },
+
+]
